206e2d1918bdc598ab43e1de4ba2e7bb40736dcd
Mudasar Shakil <117173890+mudasarasim@users.noreply.github.com>
2025-10-08 15:58:54 +0500

    ap

--- src/components/BestSellerProducts.js
+++ src/components/BestSellerProducts.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import axios from "../api/axiosConfig"; // ✅ centralized config
+import axios from "../config"; // ✅ centralized config
 import { Link } from "react-router-dom";
 
 const BestSellerProducts = () => {
