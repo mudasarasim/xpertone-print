ee5f37b0b240fb46c47b721b90e9218f92743a17
Mudasar Shakil <117173890+mudasarasim@users.noreply.github.com>
2025-10-22 20:37:56 +0500

    hj

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "http://47.129.46.103:5000"; // ✅ export string
+export const BASE_URL = "https://xpertoneprints.com"; // ✅ export string
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
